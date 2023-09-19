cf5b342dc66e60da4f2156fa974981e5f071e0e5
Adolfo-Contreras <adolfo.alvaradocontreras1760@stu.mtec.edu>
2023-09-19 08:12:00 -0600

    made all the files and starting on html

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/*.{html,js}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      flexGrow: {
+        2:'2',
+        3:'3'
+      }
+    },
+  },
+  plugins: [],
+}
+
